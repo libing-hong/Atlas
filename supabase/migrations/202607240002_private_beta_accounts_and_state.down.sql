0ffac209fc12e379b8b8c79107536c5dda5fdc3c
libing-hong <libing.hong@kedgebs.com>
2026-07-24 14:50:08 +0200

    Add Private Beta accounts and cloud state

---
+++ supabase/migrations/202607240002_private_beta_accounts_and_state.down.sql
@@ -0,0 +1,8 @@
+drop function if exists public.delete_my_atlas_account();
+drop table if exists public.product_events;
+drop table if exists public.review_queue;
+drop table if exists public.atlas_user_states;
+drop trigger if exists on_auth_user_created_create_atlas_user on auth.users;
+drop function if exists public.handle_new_atlas_auth_user();
+
+
